9a08dbbd0817ca471b0bafadb132eec574f224d9
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-10-19 11:48:56 -0400

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,13 @@
+stored =  {
+  "Spencer Karby":["Spencer Karby","l + bozo + ratio", False, "1234", 0, [], [], [], 1, False],
+  "Owen Reaser":["Owen Reaser","academic weapon", True, "7676", 0, [], [], [], 8, "bestatcoding"],
+  "Micheal Tomkzak":["Micheal Tomkzak","Genius", False, "8888", 0, [], [], [], 1, False],
+  "Parker Dillon":["Parker Dillon","69420", False, "1234", 9, [], [], [], 1, False],
+  "Walker Spittal":["Walker Spittal","Hamilton is awesome", True, "7727", 0, [], [], [], 8, "jackedness"],
+  "Avery Lassiter":["Avery Lassiter","coolest person alive", True, "0911", 0, [], [], [], 4, "jackedness"],
+}
+admins = [
+  "Walker Spittal",
+  "Avery Lassiter",
+  "Owen Reaser",
+]
